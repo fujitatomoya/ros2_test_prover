19279999c8bb837fe2621d80050d484765d16e5d
Tomoya Fujita <Tomoya.Fujita@sony.com>
2021-05-06 18:08:55 +0900

    add rclcpp::spin().
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- prover_rclcpp/src/rclcpp_1656.cpp
+++ prover_rclcpp/src/rclcpp_1656.cpp
@@ -6,5 +6,6 @@ int main(int argc, char ** argv)
   rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("my_node", "/ns");
   //rclcpp::Node::SharedPtr subnode = node->create_sub_node("sub_ns");
   rclcpp::Node::SharedPtr subnode = node->create_sub_node("");
+  rclcpp::spin(node);
   rclcpp::shutdown();
 }
