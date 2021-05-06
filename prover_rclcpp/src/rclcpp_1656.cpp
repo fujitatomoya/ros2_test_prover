6511e8f08aa0f5e1f02dbf09cf4727d4b0cbb00f
Tomoya Fujita <Tomoya.Fujita@sony.com>
2021-05-06 17:09:42 +0900

    Node::get_effective_namespace returns invaid namespace if sub-namespace is empty
      https://github.com/ros2/rclcpp/issues/1656
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

---
+++ prover_rclcpp/src/rclcpp_1656.cpp
@@ -0,0 +1,10 @@
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("my_node", "/ns");
+  //rclcpp::Node::SharedPtr subnode = node->create_sub_node("sub_ns");
+  rclcpp::Node::SharedPtr subnode = node->create_sub_node("");
+  rclcpp::shutdown();
+}
