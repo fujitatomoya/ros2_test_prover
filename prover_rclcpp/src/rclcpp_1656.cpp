51382a162a71b53df33df822293559be6a144990
Tomoya Fujita <Tomoya.Fujita@sony.com>
2021-05-07 09:12:01 +0900

    add reproducible test sample for rclcpp issue#1656
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- prover_rclcpp/src/rclcpp_1656.cpp
+++ prover_rclcpp/src/rclcpp_1656.cpp
@@ -4,8 +4,11 @@ int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("my_node", "/ns");
-  //rclcpp::Node::SharedPtr subnode = node->create_sub_node("sub_ns");
-  rclcpp::Node::SharedPtr subnode = node->create_sub_node("");
-  rclcpp::spin(node);
+  RCLCPP_INFO(node->get_logger(),
+    "Parent Node's effective name: %s", node->get_effective_namespace().c_str());
+  rclcpp::Node::SharedPtr subnode = node->create_sub_node("sub_ns");
+  //rclcpp::Node::SharedPtr subnode = node->create_sub_node(""); // generate exception here.
+  RCLCPP_INFO(node->get_logger(),
+    "Child Node's effective name: %s", subnode->get_effective_namespace().c_str());
   rclcpp::shutdown();
 }
