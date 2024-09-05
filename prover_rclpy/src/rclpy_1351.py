cdb24404302037992e1127135c09c1e4ef2f74c4
Tomoya Fujita <Tomoya.Fujita@sony.com>
2024-09-05 11:47:42 -0700

    rclpy_1351 uses ThreadPoolExecutor
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- prover_rclpy/src/rclpy_1351.py
+++ prover_rclpy/src/rclpy_1351.py
@@ -1,6 +1,8 @@
 import threading
 import time
 
+from concurrent.futures import ThreadPoolExecutor
+
 import rclpy
 from rclpy.executors import Executor
 from rclpy.executors import MultiThreadedExecutor
@@ -16,20 +18,27 @@ def main(args=None):
     future2 = Future(executor=executor)
 
     # I believe that we need to use ThreadPoolExecutor here...
-    thread1 = threading.Thread(target=executor.spin_until_future_complete,
-                                args=(future1, 10))
-    thread2 = threading.Thread(target=executor.spin_until_future_complete,
-                                args=(future2, 10))
+    #thread1 = threading.Thread(target=executor.spin_until_future_complete, args=(future1, 10))
+    #thread2 = threading.Thread(target=executor.spin_until_future_complete, args=(future2, 10))
 
-    thread1.start()
-    time.sleep(0.5)
-    thread2.start()
+    # rolling does not fail with this...
+    with ThreadPoolExecutor(max_workers=5) as exe:
+        exe.submit(executor.spin_until_future_complete, future1, 10)
+        exe.submit(executor.spin_until_future_complete, future2, 10)
 
-    future1.set_result(True)
-    future2.set_result(True)
+        time.sleep(1.0)
+        future1.set_result(True)
+        future2.set_result(True)
 
-    thread1.join()
-    thread2.join()
+    #thread1.start()
+    #time.sleep(0.5)
+    #thread2.start()
+
+    #future1.set_result(True)
+    #future2.set_result(True)
+
+    #thread1.join()
+    #thread2.join()
     executor.shutdown()
 
 
